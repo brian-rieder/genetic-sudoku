0228a05574788e8a163134f23a6b8d2821a8ca90
Brian Rieder <brian.rieder777@gmail.com>
2019-11-04 12:59:01 -0500

    Added mutation and selection operators.

--- sudoku.py
+++ sudoku.py
@@ -1,41 +1,128 @@
 #!/usr/bin/env python3.7
 
 from copy import deepcopy
-from random import sample, seed
+import matplotlib.pyplot as plt
+from random import randint, sample, seed
 from statistics import mean
 
 
 ############################################### GENETIC ALGORITHM ###############################################
 
 
+def how_many_missing(num_array):
+    flags = [1] * len(num_array)
+    for num_to_check in range(1, len(num_array)+1):
+        if num_to_check in num_array:
+            flags[num_to_check-1] = 0
+    return sum(flags)
+
+
 def fitness_function(board):
     """
     Determines the fitness value (i.e., how close it is to a final solution) of the given board. This is done via
-    averaging all of the dimensions of the board that need to add up to a final number mean(range(1,n**2+1))
+    determination of the number of errors that exist in the current board.
     :param board: the board whose fitness is being evaluated
-    :return: float value representing the fitness of the given board
+    :return: integer value representing the fitness of the given board
     """
     side_length = len(board)
     base = int(side_length ** 0.5)
-    # sum each row
-    row_sum = sum([sum(row) for row in board])
-    # sum each column
-    column_sum = sum([sum(column) for column in zip(*board)])
-    # sum each interior box
+    # check errors in each row
+    row_sum = sum([how_many_missing(row) for row in board])
+    # check errors in each column
+    column_sum = sum([how_many_missing(column) for column in zip(*board)])
+    # check errors in each interior box
     box_sum = 0
     for row_idx in range(0, base):
         for col_idx in range(0, base):
             box = [row[row_idx*base:(row_idx+1)*base] for row in board[col_idx*base:(col_idx+1)*base]]
-            box_sum += sum(sum(box, []))
-    # find the average value to determine fitness
-    board_fitness = (row_sum + column_sum + box_sum) / (side_length * 3)
-    # determine and return the variation from the ideal fitness value
-    ideal_fitness = mean(range(1,n**2+1)) * side_length
-    return abs(ideal_fitness - board_fitness)
+            box_sum += how_many_missing(sum(box, []))
+    # return the total number of errors
+    return row_sum + column_sum + box_sum
 
 
-def mutation_operator(board, removal_map):
-    pass
+def get_best_fitness(board_list):
+    """
+    Returns the best (lowest) fitness value of the boards being analyzed.
+    :param board_list: the list of boards being analyzed
+    :return: the float fitness value of the best board
+    """
+    return min([fitness_function(board) for board in board_list])
+
+
+def get_best_board(board_list):
+    """
+    Returns the best board (i.e., the board that has the lowest fitness value)
+    :param board_list: the list of boards being analyzed
+    :return: the board the had the lowest fitness value
+    """
+    fitnesses = [fitness_function(board) for board in board_list]
+    return board_list[fitnesses.index(min(fitnesses))]
+
+
+def mutation_operator_old(board_list, removal_map):
+    """
+    Mutates the provided list of boards by incrementing the least mutated puzzle index in a random row.
+    :param board_list: the list of puzzle boards
+    :param removal_map: the mapping which indicates which indices need to be updated
+    :return: updated list of mutated boards
+    """
+    board_size = len(board_list)
+    for board in board_list:
+        row = randint(0, board_size-1)
+        col = removal_map[row].index(min(x for x in removal_map[row] if x > 0))
+        board[row][col] = ((board[row][col] + 1) % board_size) + 1
+        removal_map[row][col] += 1
+    return board_list, removal_map
+
+
+def mutation_operator(board_list, removal_map):
+    """
+    Mutates the provided list of boards by incrementing the least mutated puzzle index in a random row.
+    :param board_list: the list of puzzle boards
+    :param removal_map: the mapping which indicates which indices need to be updated
+    :return: updated list of mutated boards
+    """
+    fitness_list = [fitness_function(board) for board in board_list]
+    initial_fitness = max(fitness_list)
+    worst_board = board_list[fitness_list.index(initial_fitness)]
+    board_size = len(worst_board)
+    row = randint(0, board_size-1)
+    if sum(removal_map[row]) == 0:
+        for i in range(board_size):
+            if sum(removal_map[i]) != 0:
+                row = i
+                break
+    col = removal_map[row].index(min(x for x in removal_map[row] if x > 0))
+    worst_board[row][col] = (worst_board[row][col] + 1) % board_size
+    if worst_board[row][col] == 0:
+        worst_board[row][col] = board_size
+    removal_map[row][col] += 1
+    # evaluate fitness and mark "done" if it's correct
+    final_fitness = fitness_function(worst_board)
+    # if the row, col, and box are all correct now, we're done with this square
+    if (final_fitness + 3) == initial_fitness:
+        removal_map[row][col] = 0
+        for board in board_list:
+            board[row][col] = worst_board[row][col]
+    return board_list, removal_map
+
+
+def crossover_operator(board_list, removal_map):
+    board_size = len(board_list[0])
+    # determine the fitness of each board
+    fitness_values = [fitness_function(board) for board in board_list]
+    # we'll iterate until we've gotten everything
+    while sum(fitness_values) > 0:
+        least_fit_idx = fitness_values.index(max(fitness_values))
+        most_fit_idx = fitness_values.index(min(x for x in fitness_values if x > 0))
+        least_fit_idx = least_fit_idx if least_fit_idx != most_fit_idx else (least_fit_idx + 1) % board_size
+        fitness_values[least_fit_idx] = 0
+        fitness_values[most_fit_idx] = 0
+        # select a position to crossover
+        row = randint(0, board_size-1)
+        col = removal_map[row].index(max(removal_map[row]))
+        board_list[least_fit_idx][row][col] = board_list[most_fit_idx][row][col]
+    return board_list
 
 
 ############################################### SUDOKU GENERATION ###############################################
@@ -104,12 +191,36 @@ def pretty_print(board):
 
 
 if __name__ == "__main__":
-    seed(724) # DEVELOPER USE
     n = 3  # n is meant to be the dimension of the interior boxes, i.e. the board is n^2 * n^2
+    population_size = 10
+    # generate the sudoku board
     solved_board = generate_sudoku(n)
-    pretty_print(solved_board)
+    # generate the puzzle board and the map of where we removed values
     puzzle_board, removed_map = generate_removal_map(solved_board)
-    print()
+    # create an array of this puzzle board to form the population
+    puzzle_board_list = [deepcopy(puzzle_board) for _ in range(1, population_size)]
+
     pretty_print(puzzle_board)
-    print()
-    mutation_operator(puzzle_board)
+    fitness_function(puzzle_board)
+    # fig = plt.gcf()
+    # fig.show()
+    # fig.canvas.draw()
+    time = 0
+    best_fitness = -1
+    while best_fitness != 0:
+        # mutate the list
+        puzzle_board_list, removed_map = mutation_operator(puzzle_board_list, removed_map)
+        # crossover the list
+        puzzle_board_list = crossover_operator(puzzle_board_list, removed_map)
+        # plot the fitness value
+        best_fitness = get_best_fitness(puzzle_board_list)
+        print(str(time) + ": " + str(best_fitness))
+        # plt.scatter(time, best_fitness)
+        # plt.pause(0.01)
+        # fig.canvas.draw()
+        time += 1
+    plt.ioff()
+    plt.show()
+    solution = get_best_board(puzzle_board_list)
+    pretty_print(solved_board)
+    pretty_print(solution)
